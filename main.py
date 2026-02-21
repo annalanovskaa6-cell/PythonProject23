558fc7178d4216e5718827020c5b9e1aacd0d504
anya_lanovsky <annalanovskaa6@gmail.com>
2026-02-21 17:59:27 +0200

    hh

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import requests
 from config import Config, load_config
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
+from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 config: Config = load_config()
 bot_token = config.bot.token
@@ -11,14 +11,32 @@ dp = Dispatcher()
 @dp.message(Command(commands=['start']))
 async def process_start(message: Message):
     await message.answer("Привет! Я твой первый бот")
-#help
+
 @dp.message(Command(commands=['help']))
 async def process_help(message: Message):
-    await message.answer("Чем тебе помочь?")
+    await message.answer("Команды:\n/start - Старт \n/help - О боте")
+
+@dp.message(Command(commands=['breeds']))
+async def show_breeds(message: Message):
+    result = requests.get("https://dog.ceo/api/breeds/list/all")
+    result_json = result.json()
+    dogs_types = result_json["message"].keys()
+    s = "\n".join(list(dogs_types)[:30])
+    await message.answer(s)
+
 @dp.message(Command(commands=['dog']))
-async def process_dog(message: Message):
-    request = requests.get("https://dog.ceo/api/breeds/image/random")
-    await message.answer(request.text)
+async def process_dog(message: Message, command: CommandObject):
+    if command.args:
+        s = requests.get(f"https://dog.ceo/api/breed/{command.args}/images/random")
+    else:
+        s = requests.get(f"https://dog.ceo/api/breeds/image/random")
+    json_s = s.json()
+    if json_s.get("status") == "success":
+        await message.answer_photo(photo = json_s.get("message"))
+    else:
+        await message.answer("Ты не прав, я не могу так")
+    print(s.json())
+
 @dp.message()
 async def send_echo(message: Message):
     await message.reply(message.text)
