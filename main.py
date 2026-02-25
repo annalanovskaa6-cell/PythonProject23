f1a1406a06ec95fc7f8228ed463acf1658d0187a
anya_lanovsky <annalanovskaa6@gmail.com>
2026-02-25 19:42:26 +0200

    hhh

--- main.py
+++ main.py
@@ -1,46 +1,50 @@
 import asyncio
-import requests
 from config import Config, load_config
-from aiogram import Bot, Dispatcher
-from aiogram.filters import Command, CommandObject
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, FSInputFile
+from pathlib import Path
+
 config: Config = load_config()
 bot_token = config.bot.token
 bot = Bot(token=bot_token)
 dp = Dispatcher()
-@dp.message(Command(commands=['start']))
-async def process_start(message: Message):
-    await message.answer("Привет! Я твой первый бот")
 
-@dp.message(Command(commands=['help']))
-async def process_help(message: Message):
-    await message.answer("Команды:\n/start - Старт \n/help - О боте")
+# #домашняя папка пользователя
+# HOME_DIR = Path.home()
+# #рабочий стол
+# DESKTOP_DIR = HOME_DIR / 'Desktop'
+# #папка downloads
+# DOWNLOAD_DIR = HOME_DIR / 'downloads'
 
-@dp.message(Command(commands=['breeds']))
-async def show_breeds(message: Message):
-    result = requests.get("https://dog.ceo/api/breeds/list/all")
-    result_json = result.json()
-    dogs_types = result_json["message"].keys()
-    s = "\n".join(list(dogs_types)[:30])
-    await message.answer(s)
+# @dp.message(F.photo)
+# async def download_photo(message: Message, bot: Bot):
+#     photo = message.photo[-1]
+#     file = await bot.get_file(photo.file_id)
+#     await bot.download_file(file.file_path, "/")
 
-@dp.message(Command(commands=['dog']))
-async def process_dog(message: Message, command: CommandObject):
-    if command.args:
-        s = requests.get(f"https://dog.ceo/api/breed/{command.args}/images/random")
-    else:
-        s = requests.get(f"https://dog.ceo/api/breeds/image/random")
-    json_s = s.json()
-    if json_s.get("status") == "success":
-        await message.answer_photo(photo = json_s.get("message"))
-    else:
-        await message.answer("Ты не прав, я не могу так")
-    print(s.json())
-
-@dp.message()
-async def send_echo(message: Message):
-    await message.reply(message.text)
+@dp.message(F.photo)
+async def send_photo_url(message: Message):
+    # photo = "https://glstatic.rg.ru/uploads/images/2016/03/05/86a8c745a45790e.jpg"
+    # await message.answer_photo(
+    #     photo,
+    #     caption= "Фото кота"
+    # )
+    #отправка локального файла
+    # photo = FSInputFile("files/test_1.jpg")
+    # file = await bot.get_file(photo.file_id)
+    # await message.reply_photo(
+    #     photo = photo,
+    #     caption = "Вот сохранённое фото"
+    # )
+@dp.message(F.photo)
+async def reply_photo(message: Message):
+    photo = message.photo[-1]
+    await message.answer_photo(
+        photo = photo.file_id,
+        caption = "Не присылай больше"
+    )
 async def main():
     await dp.start_polling(bot)
+
 if __name__ == '__main__':
     asyncio.run(main())
