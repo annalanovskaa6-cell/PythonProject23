8cac76e4e5d9809adb2ca7c5017bdfc7a1fad895
anya_lanovsky <annalanovskaa6@gmail.com>
2026-04-04 19:46:04 +0200

    hhh

--- main.py
+++ main.py
@@ -18,15 +18,41 @@ async def main():
     )
     questions = [
         {"Кто выиграл чемпионат мира по футболу в 2022 году?":
-             {"Аргентина": True,
-              "Испания": False,
-              "Франция": False
+             {
+                 "Аргентина": True,
+                 "Испания": False,
+                 "Франция": False
               }
          },
-        {"В каком году произошло крещение Руси?": "988 год"},
-        {"Кто был последним императором Римской империи перед её падением?": "Флавий Ромул Август"}
+        {"В каком году произошло крещение Руси?":
+             {
+                 "988 год": True,
+                 "856 год": False,
+                 "1048 год": False
+             }
+         },
+        {"Кто был последним императором Римской империи перед её падением?":
+            {
+                "Флавий Ромул Август ": True,
+                "Гай Юлий Цезарь": False,
+                "Октавиан Август": False
+             }
+         }
     ]
     counter = 0
+    user_answ = []
+    async def get_answ_btns(btns: dict):
+        s = []
+        for i, (key, value) in enumerate(btns.items()):
+            btn = InlineKeyboardButton(
+                text = key,
+                callback_data = f"question_{i}"
+            )
+            s.append([btn])
+        answ_keyboard = InlineKeyboardMarkup(
+            inline_keyboard = s
+        )
+        return answ_keyboard
     @dp.message(Command(commands="start"))
     async def start_handler(message: Message):
         await message.answer(
@@ -35,41 +61,36 @@ async def main():
         )
     @dp.message(F.text == "Начать тест")
     async def start_test_handler(message: Message):
-        counter = 0
-        answ = list(questions[counter].keys())
-        text = list(questions[0].values())[0]
-        answ_1_btn = InlineKeyboardButton(
-            text = text,
-            callback_data = "question"
-        )
-        answ_keyboard = InlineKeyboardMarkup(
-            inline_keyboard=[[answ_1_btn]]
-        )
+        quest = list(questions[0].keys())[0]
+        btns_data = questions[0][quest]
+        keyboard = await get_answ_btns(btns_data)
         await message.answer(
-            answ[0],
-            reply_markup = answ_keyboard
+            quest,
+            reply_markup = keyboard
         )
     @dp.callback_query(F.data.startswith("question"))
     async def answ_handler(callback: CallbackQuery):
-        nonlocal counter
+        nonlocal counter, user_answ
+        data = callback.data
+        num = int(data.split("_")[1])
+        user_answ.append(num)
         counter += 1
-        answ = list(questions[counter].keys())[0]
-        text = list(questions[counter].values())[0]
-        answ_1_btn = InlineKeyboardButton(
-            text=text,
-            callback_data="question"
+        if counter == 3:
+            s = ""
+            for i in s:
+
+            # 1 вопрос
+            # Какой результат деления на 10
+            # ответ : {ответ пользователя}
+            await callback.message.answer("")
+        else:
+            answ = list(questions[counter].keys())[0]
+            btns_data= list(questions[counter].values())[0]
+            keyboard = await get_answ_btns(btns_data)
+            await callback.message.answer(
+                text = answ,
+                reply_markup = keyboard
         )
-        answ_keyboard = InlineKeyboardMarkup(
-            inline_keyboard=[[answ_1_btn]]
-        )
-        await callback.message.answer(
-            text = answ,
-            reply_markup = answ_keyboard
-        )
-
-
-
-
 
 
 
